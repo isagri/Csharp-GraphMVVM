7c69377c3ca6447f23feca90ec9f2ed628d499a5
isagri <isabelle.grillet6@orange.fr>
2018-06-29 15:46:18 +0200

    appGraphMVVM Transport Grenoble

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TransportLinesLib;
+
+namespace WpfApp2_MVVM
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel1 viewModel1;
+
+        public MainWindow()
+        {
+            // variable d'environnement CurrentCulture -> en-US pour que la virgule décimale soit en point lorsqu'on convertit un integer en string (appelle à l'api Métro)
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+            InitializeComponent();
+
+            viewModel1 = new ViewModel1();
+            base.DataContext = viewModel1;
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel1.appelSearchLineStop();
+            //tls.searchLineStop(Convert.ToDouble(lat.Text), Convert.ToDouble(lon.Text), Convert.ToInt16(dist.Text));
+            //gridtls.ItemsSource = tls.lStops;
+
+        }
+
+        private void gridtls_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var grid = sender as DataGrid;
+            if (grid.SelectedItems.Count > 0)
+            {
+                var selected = grid.SelectedItems[0] as LineStop;
+
+                gridtl.ItemsSource = selected.tlines;
+                tlsName.Text = selected.id + " " + selected.name;
+            }
+            else
+            {
+                gridtl.ItemsSource = null;
+                tlsName.Text = null;
+            }
+        }
+
+    }
+}
---
+++ ViewModel1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransportLinesLib;
+
+namespace WpfApp2_MVVM
+{
+    public class ViewModel1 : INotifyPropertyChanged
+    {
+        private Double lat = 5.709360123;
+        private Double lon = 45.1764946;
+        private int dist = 400;
+
+        public double Lat { get => lat; set => lat = value; }
+        public double Lon { get => lon; set => lon = value; }
+        public int Dist { get => dist; set => dist = value; }
+
+        public ObservableCollection<LineStop> LineStopsVM { get; set; }
+
+        TransportLineStop tls;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel1()
+        {   
+            tls = new TransportLineStop();
+            //tls.searchLineStop(Convert.ToDouble(lat.Text), Convert.ToDouble(lon.Text), Convert.ToInt16(dist.Text));
+            LineStopsVM = new ObservableCollection<LineStop>();
+            appelSearchLineStop();
+        }
+
+        public void appelSearchLineStop()
+        {
+            tls.searchLineStop(Lat, Lon, Dist);
+
+            LineStopsVM.Clear();
+            foreach (LineStop lstop in tls.lStops)
+            {
+                LineStopsVM.Add(lstop);
+            }
+        }
+
+    }
+}
